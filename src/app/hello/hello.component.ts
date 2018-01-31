9a78c745dbaa33be106f21b675d0d0586c4b6b7a
mahesh147 <maheshchandran147@gmail.com>
2018-02-01 00:07:03 +0530

    Added the Hello component

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PEOPLE , Person} from '../person/person';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  greeting: string;
+  people = PEOPLE;
+  personName: Person;
+
+  constructor() {
+
+  }
+
+  addPerson(personName) {
+
+      this.people.push({name: personName})
+  }
+  ngOnInit() {
+  }
+
+}
